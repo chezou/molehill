2462fc230704bb13b6cc52c374708db749618300
Aki Ariga <ariga@treasure-data.com>
2019-02-27 17:34:25 +0900

    Add setup.py

--- README.md
+++ README.md
@@ -4,11 +4,16 @@ Combine Apache Hivemall(incubating) and digdag together.
 
 Generate Hivemall queries and Digdag workflow for TreasureData from YAML file.
 
+## Installation
+
+```bash
+$ pip install git+https://github.com/chezou/molehill#egg=molehill
+```
+
 ## Usage
 
 ```bash
-$ pip install -r requirements.txt -c constraints.txt
-$ python generate_workflow.py --overwrite -dest titanic.rf resources/titanic_pipeline.yml
+$ generate_workflow --overwrite -dest titanic.rf resources/titanic_pipeline.yml
 $ td wf push proj-name
 $ td wf start proj-name titanic --session now
 ```
---
+++ molehill/generate_workflow.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+import argparse
+from molehill.pipeline import Pipeline
+
+
+def main():
+    parser = argparse.ArgumentParser(description="Generate ML workflow from yaml file")
+    parser.add_argument('yaml', metavar='file', type=str,
+                        help='yaml file path to convert')
+    parser.add_argument('--dest', type=str,
+                        help='output file name')
+    parser.add_argument('--overwrite', action='store_true', help='overwrite existing file/dir')
+
+    args = parser.parse_args()
+
+    print("Start converting: {}".format(args.yaml))
+    pipe = Pipeline()
+    pipe.dump_pipeline(args.yaml, args.dest, overwrite=args.overwrite)
+    print("Finish dump file: {}".format(pipe.workflow_path))
+
+
+if __name__ == '__main__':
+    main()
